4ada2de5c3619bb23bc6f23dda49ac3a92acdcc6
matumolise <matumolise@gmail.com>
2024-11-14 21:28:19 -0300

    welcome again

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -42,7 +42,7 @@ const SignInScreen = ({ navigation }) => {
               style={styles.logo}
             />
             <Text style={styles.title}>REDMEDIA</Text>
-            <Text style={styles.welcomeText}>Bienvenido de nuevo</Text>
+            <Text style={styles.welcomeText}>Welcome Again</Text>
 
           <FormikInputValue name="email" placeholder="Enter your email" placeholderTextColor="#aaa" keyboardType="email-address" />
           <FormikInputValue name="password" placeholder="Enter your password" placeholderTextColor="#aaa" secureTextEntry={true} />
